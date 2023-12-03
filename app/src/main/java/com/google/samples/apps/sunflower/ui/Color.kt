1e57f4d4836d6304d8e0903f1fe6e1cd2b2d0772
Chimara Okeke <123244902+Echimara@users.noreply.github.com>
2023-12-02 18:48:55 -0600

    Add app to github

---
+++ app/src/main/java/com/google/samples/apps/sunflower/ui/Color.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower.ui
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF64B5F6) // Light Blue
+val md_theme_light_onPrimary = Color(0xFFEEFFFF) // Light Cyan
+val md_theme_light_primaryContainer = Color(0xFF81C784) // Light Green
+val md_theme_light_onPrimaryContainer = Color(0xFFAB47BC) // Purple
+val md_theme_light_secondary = Color(0xFFEF9A9A) // Light Red
+val md_theme_light_onSecondary = Color(0xFF424242) // Dark Grey
+val md_theme_light_secondaryContainer = Color(0xFFFFD54F) // Amber
+val md_theme_light_onSecondaryContainer = Color(0xFF3949AB) // Indigo
+val md_theme_light_tertiary = Color(0xFFFFCC80) // Light Orange
+val md_theme_light_onTertiary = Color(0xFF3E2723) // Brown
+val md_theme_light_tertiaryContainer = Color(0xFFDCE775) // Lime
+val md_theme_light_onTertiaryContainer = Color(0xFF006064) // Teal
+val md_theme_light_error = Color(0xFFEF5350) // Red
+val md_theme_light_errorContainer = Color(0xFFFFEBEE) // Light Red
+val md_theme_light_onError = Color(0xFFC62828) // Dark Red
+val md_theme_light_onErrorContainer = Color(0xFF8C9EFF) // Light Blue
+val md_theme_light_background = Color(0xFFFFFDE7) // Ivory
+val md_theme_light_onBackground = Color(0xFF263238) // Dark Blue Grey
+val md_theme_light_surface = Color(0xFFFFFDE7) // Ivory
+val md_theme_light_onSurface = Color(0xFF263238) // Dark Blue Grey
+val md_theme_light_surfaceVariant = Color(0xFFE0E0E0) // Grey
+val md_theme_light_onSurfaceVariant = Color(0xFF455A64) // Blue Grey
+val md_theme_light_outline = Color(0xFF90A4AE) // Blue Grey
+val md_theme_light_inverseOnSurface = Color(0xFFF9FBE7) // Off White
+val md_theme_light_inverseSurface = Color(0xFF263238) // Dark Blue Grey
+val md_theme_light_inversePrimary = Color(0xFF64B5F6) // Light Blue
+val md_theme_light_shadow = Color(0xFF000000) // Black
+val md_theme_light_surfaceTint = Color(0xFF64B5F6) // Light Blue
+val md_theme_light_outlineVariant = Color(0xFFB0BEC5) // Blue Grey
+val md_theme_light_scrim = Color(0xFF000000) // Black
+
+val md_theme_dark_primary = Color(0xFFFF80AB) // Pink
+val md_theme_dark_onPrimary = Color(0xFF1B0000) // Dark Maroon
+val md_theme_dark_primaryContainer = Color(0xFF4A148C) // Dark Purple
+val md_theme_dark_onPrimaryContainer = Color(0xFFC51162) // Pink
+val md_theme_dark_secondary = Color(0xFFFF5252) // Light Red
+val md_theme_dark_onSecondary = Color(0xFF1A237E) // Dark Indigo
+val md_theme_dark_secondaryContainer = Color(0xFFD32F2F) // Dark Red
+val md_theme_dark_onSecondaryContainer = Color(0xFF536DFE) // Blue
+val md_theme_dark_tertiary = Color(0xFFFFD180) // Light Orange
+val md_theme_dark_onTertiary = Color(0xFF3E2723) // Brown
+val md_theme_dark_tertiaryContainer = Color(0xFFFFE082) // Light Orange
+val md_theme_dark_onTertiaryContainer = Color(0xFF00695C) // Teal
+val md_theme_dark_error = Color(0xFFFF8A80) // Light Red
+val md_theme_dark_errorContainer = Color(0xFF311B92) // Dark Purple
+val md_theme_dark_onError = Color(0xFFD50000) // Red
+val md_theme_dark_onErrorContainer = Color(0xFFFFD180) // Light Orange
+val md_theme_dark_background = Color(0xFF1A1A1D) // Dark Grey
+val md_theme_dark_onBackground = Color(0xFFF9FBE7) // Off White
+val md_theme_dark_surface = Color(0xFF1A1A1D) // Dark Grey
+val md_theme_dark_onSurface = Color(0xFFF9FBE7) // Off White
+val md_theme_dark_surfaceVariant = Color(0xFF3E3E40) // Grey
+val md_theme_dark_onSurfaceVariant = Color(0xFFB0BEC5) // Blue Grey
+val md_theme_dark_outline = Color(0xFF546E7A) // Blue Grey
+val md_theme_dark_inverseOnSurface = Color(0xFF1A1A1D) // Dark Grey
+val md_theme_dark_inverseSurface = Color(0xFFF9FBE7) // Off White
+val md_theme_dark_inversePrimary = Color(0xFFFF80AB) // Pink
+val md_theme_dark_shadow = Color(0xFF000000) // Black
+val md_theme_dark_surfaceTint = Color(0xFFFF80AB) // Pink
+val md_theme_dark_outlineVariant = Color(0xFF3E3E40) // Grey
+val md_theme_dark_scrim = Color(0xFF000000) // Black
+
+val seed = Color(0xFF256F00) // Green
+
